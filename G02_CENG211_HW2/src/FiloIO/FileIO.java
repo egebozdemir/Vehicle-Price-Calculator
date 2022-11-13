1753a8ee098f4b1d9132ff2a08bc11da2744bac2
KaganAsl <kagan.aslancan@gmail.com>
2022-11-13 18:39:35 +0300

    FileIO methods
    FileIO class methods created.

--- G02_CENG211_HW2/src/FiloIO/FileIO.java
+++ G02_CENG211_HW2/src/FiloIO/FileIO.java
@@ -1,5 +1,49 @@
 package FiloIO;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import Vehicles.*;
+import Vehicles.SoldVehiclesSub.*;
+import Vehicles.SoldVehiclesSub.AutomobileSub.*;
+
 public class FileIO {
+	public final static String PATH_TO_CSV = "Csv/HW2_SoldVehicles.csv";
 	
+	/* Csv reading */
+	private static BufferedReader readCsv(String pathToFile) throws FileNotFoundException {
+		return new BufferedReader(new FileReader(pathToFile));
+	}
+	public static ArrayList<SoldVehicles> createVehicleListFromCsv(String pathToFile) throws IOException {
+		BufferedReader reader = readCsv(pathToFile);
+		ArrayList<SoldVehicles> vehicleArrayList = new ArrayList<>();
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			vehicleArrayList.add(createVehiceFromCsv(line));
+		}
+		return vehicleArrayList;
+	}
+	
+	/* Object creation from csv lines */	
+	private static SoldVehicles createVehiceFromCsv(String csvLine) {
+		StringTokenizer tokenizer = new StringTokenizer(csvLine, ",");
+		switch (csvLine.charAt(0)) {
+			case 'B':
+				Bicycle bicycle = new Bicycle(tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken());
+				return bicycle; // We didn't use break statement beacuse we use return statement in every case.
+			case 'P':
+				PickupTruck pickupTruck = new PickupTruck(tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken());
+				return pickupTruck;
+			case 'H':
+				Hatchback hatchback = new Hatchback(tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken());
+				return hatchback;
+			case 'M':
+				Minivan minivan = new Minivan(tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken());
+				return minivan;
+			case 'S':
+				Sedan sedan = new Sedan(tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken());
+				return sedan;
+		}
+		return null;
+	}
 }
